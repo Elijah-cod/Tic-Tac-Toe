178d324e8a0d605ad605b2c58e4369d336ebaeba
Elijah <iamngunjiri@gmail.com>
2023-09-06 09:19:39 +0300

    Created a working skeleton of the tic tac toe game

---
+++ script.js
@@ -0,0 +1,10 @@
+// Create an object with an array property
+const Gameboard = {
+    gameBoard: [1, 2, 3, 4, 5]
+};
+
+//Create a Player object
+const Player = {}
+
+//Object controlling the flow of the game
+const Gameflow = {}
